6569fdb4ce54f07416c46a208ddb0450835c07b0
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-11-27 18:02:41 +0800

    op update

---
+++ screens/reviewsDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { globalStyles } from "../styles/global";
+
+const ReviewDetails = ({ navigation }) => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>{navigation.getParam("title")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+      <Text>{navigation.getParam("rating")}</Text>
+    </View>
+  );
+};
+
+export default ReviewDetails;
