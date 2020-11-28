f52d9fb879577b93c8e505010d235d8a1058a5f5
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-11-28 13:21:25 +0800

    form validation

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
+import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "./reviewForm";
 
 const Home = ({ navigation }) => {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     {
       title: "Zelda, Breath of Fresh Air",
@@ -25,8 +37,36 @@ const Home = ({ navigation }) => {
     },
   ]);
 
+  const addReview = (review) => {
+    // lousy key
+    review.key = Math.random().toString();
+    setReviews((prevState) => {
+      return [review, ...prevState];
+    });
+    setModalOpen(false);
+  };
+
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <MaterialIcons
+              name="close"
+              size={24}
+              onPress={() => setModalOpen(false)}
+              style={{ ...styles.modalTogger, ...styles.modalClose }}
+            />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <MaterialIcons
+        name="add"
+        size={24}
+        onPress={() => setModalOpen(true)}
+        style={styles.modalTogger}
+      />
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -42,5 +82,21 @@ const Home = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modalTogger: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: { flex: 1 },
+});
 
 export default Home;
---
+++ screens/reviewForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { globalStyles } from "../styles/global.js";
+import { Formik } from "formik";
+import * as yup from "yup";
+
+const ReviewSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("is-num-1-5", "Rating must be a number 1 - 5", (value) => {
+      return parseInt(value) < 6 && parseInt(value) > 0;
+    }),
+});
+
+const ReviewForm = ({ addReview }) => {
+  return (
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ title: "", body: "", rating: "" }}
+        validationSchema={ReviewSchema}
+        onSubmit={(values, actions) => {
+          addReview(values);
+        }}
+      >
+        {(formikProps) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review Title"
+              onChangeText={formikProps.handleChange("title")}
+              value={formikProps.values.title}
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.title && formikProps.errors.title}
+            </Text>
+            <TextInput
+              multiline
+              style={globalStyles.input}
+              placeholder="Review body"
+              onChangeText={formikProps.handleChange("body")}
+              value={formikProps.values.body}
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.body && formikProps.errors.body}
+            </Text>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1-5)"
+              onChangeText={formikProps.handleChange("rating")}
+              value={formikProps.values.rating}
+              keyboardType="numeric"
+            />
+            <Text style={globalStyles.errorText}>
+              {formikProps.touched.errors && formikProps.errors.rating}
+            </Text>
+            <Button
+              title="submit"
+              color="maroon"
+              onPress={formikProps.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+export default ReviewForm;
